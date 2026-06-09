2b19e7c9ac7cd187a7179455be94bc065ab93c1c
Harsh Singh <harshsinghps57@gmail.com>
2026-06-09 13:41:02 +0530

    Upload dashboard/data.js - Week 3 Deliverable

---
+++ dashboard/data.js
@@ -0,0 +1,953 @@
+// Pre-calculated data generated from Superstore dataset for Week 3 BI Dashboard
+const week3Data = {
+  "kpis": {
+    "sales": 2297200.86,
+    "profit": 286397.02,
+    "aov": 458.61,
+    "purchase_frequency": 6.32,
+    "total_customers": 793,
+    "overall_retention": 98.49
+  },
+  "cohorts": {
+    "labels": [
+      "2014Q1",
+      "2014Q2",
+      "2014Q3",
+      "2014Q4",
+      "2015Q1",
+      "2015Q2",
+      "2015Q3",
+      "2015Q4",
+      "2016Q1",
+      "2016Q2",
+      "2016Q3",
+      "2016Q4",
+      "2017Q1",
+      "2017Q2",
+      "2017Q3",
+      "2017Q4"
+    ],
+    "retention": [
+      {
+        "cohort": "2014Q1",
+        "values": [
+          100.0,
+          16.5,
+          23.1,
+          33.9,
+          14.9,
+          22.3,
+          27.3,
+          37.2,
+          21.5,
+          28.1,
+          35.5,
+          43.8,
+          28.1,
+          40.5,
+          42.1,
+          54.5
+        ]
+      },
+      {
+        "cohort": "2014Q2",
+        "values": [
+          100.0,
+          24.4,
+          36.2,
+          17.5,
+          27.5,
+          29.4,
+          42.5,
+          20.6,
+          31.9,
+          35.6,
+          48.8,
+          23.1,
+          36.2,
+          45.0,
+          53.1,
+          null
+        ]
+      },
+      {
+        "cohort": "2014Q3",
+        "values": [
+          100.0,
+          32.9,
+          18.6,
+          28.6,
+          29.8,
+          41.6,
+          18.6,
+          29.8,
+          39.8,
+          43.5,
+          23.0,
+          39.1,
+          44.7,
+          52.2,
+          null,
+          null
+        ]
+      },
+      {
+        "cohort": "2014Q4",
+        "values": [
+          100.0,
+          17.6,
+          20.3,
+          31.4,
+          39.9,
+          19.6,
+          32.7,
+          37.9,
+          41.2,
+          27.5,
+          36.6,
+          41.8,
+          62.1,
+          null,
+          null,
+          null
+        ]
+      },
+      {
+        "cohort": "2015Q1",
+        "values": [
+          100.0,
+          25.0,
+          31.2,
+          25.0,
+          18.8,
+          18.8,
+          37.5,
+          50.0,
+          37.5,
+          37.5,
+          21.9,
+          50.0,
+          null,
+          null,
+          null,
+          null
+        ]
+      },
+      {
+        "cohort": "2015Q2",
+        "values": [
+          100.0,
+          30.6,
+          38.9,
+          27.8,
+          27.8,
+          44.4,
+          44.4,
+          36.1,
+          19.4,
+          55.6,
+          58.3,
+          null,
+          null,
+          null,
+          null,
+          null
+        ]
+      },
+      {
+        "cohort": "2015Q3",
+        "values": [
+          100.0,
+          41.7,
+          27.8,
+          22.2,
+          38.9,
+          44.4,
+          25.0,
+          52.8,
+          52.8,
+          41.7,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null
+        ]
+      },
+      {
+        "cohort": "2015Q4",
+        "values": [
+          100.0,
+          9.4,
+          15.6,
+          34.4,
+          37.5,
+          37.5,
+          18.8,
+          40.6,
+          53.1,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null
+        ]
+      },
+      {
+        "cohort": "2016Q1",
+        "values": [
+          100.0,
+          42.9,
+          42.9,
+          50.0,
+          57.1,
+          42.9,
+          64.3,
+          50.0,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null
+        ]
+      },
+      {
+        "cohort": "2016Q2",
+        "values": [
+          100.0,
+          59.1,
+          22.7,
+          36.4,
+          22.7,
+          40.9,
+          59.1,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null
+        ]
+      },
+      {
+        "cohort": "2016Q3",
+        "values": [
+          100.0,
+          null,
+          16.7,
+          50.0,
+          16.7,
+          66.7,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null
+        ]
+      },
+      {
+        "cohort": "2016Q4",
+        "values": [
+          100.0,
+          22.2,
+          11.1,
+          22.2,
+          77.8,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null
+        ]
+      },
+      {
+        "cohort": "2017Q1",
+        "values": [
+          100.0,
+          33.3,
+          33.3,
+          33.3,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null
+        ]
+      },
+      {
+        "cohort": "2017Q2",
+        "values": [
+          100.0,
+          null,
+          50.0,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null
+        ]
+      },
+      {
+        "cohort": "2017Q3",
+        "values": [
+          100.0,
+          33.3,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null
+        ]
+      },
+      {
+        "cohort": "2017Q4",
+        "values": [
+          100.0,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null
+        ]
+      }
+    ],
+    "ltv": [
+      {
+        "cohort": "2014Q1",
+        "values": [
+          615.27,
+          698.36,
+          882.17,
+          1182.37,
+          1228.27,
+          1347.36,
+          1485.85,
+          1741.6,
+          1873.51,
+          2024.84,
+          2193.66,
+          2503.33,
+          2645.5,
+          2847.64,
+          3038.13,
+          3353.04
+        ]
+      },
+      {
+        "cohort": "2014Q2",
+        "values": [
+          478.03,
+          592.56,
+          835.56,
+          903.97,
+          1082.27,
+          1262.99,
+          1503.58,
+          1670.23,
+          1900.72,
+          2070.87,
+          2352.8,
+          2441.21,
+          2612.99,
+          2812.43,
+          3159.35,
+          3159.35
+        ]
+      },
+      {
+        "cohort": "2014Q3",
+        "values": [
+          640.18,
+          785.73,
+          894.87,
+          987.39,
+          1121.53,
+          1404.9,
+          1488.62,
+          1593.61,
+          1756.68,
+          2069.89,
+          2132.32,
+          2325.1,
+          2606.35,
+          2991.28,
+          2991.28,
+          2991.28
+        ]
+      },
+      {
+        "cohort": "2014Q4",
+        "values": [
+          529.33,
+          641.45,
+          727.0,
+          933.72,
+          1123.51,
+          1244.39,
+          1386.93,
+          1550.16,
+          1906.52,
+          2131.59,
+          2285.91,
+          2536.71,
+          2905.32,
+          2905.32,
+          2905.32,
+          2905.32
+        ]
+      },
+      {
+        "cohort": "2015Q1",
+        "values": [
+          550.87,
+          738.82,
+          893.02,
+          1070.82,
+          1177.67,
+          1192.73,
+          1298.68,
+          1623.15,
+          1925.68,
+          2074.04,
+          2418.92,
+          2787.53,
+          2787.53,
+          2787.53,
+          2787.53,
+          2787.53
+        ]
+      },
+      {
+        "cohort": "2015Q2",
+        "values": [
+          338.5,
+          569.56,
+          797.87,
+          977.8,
+          1357.66,
+          1684.36,
+          1991.73,
+          2092.62,
+          2107.44,
+          2318.01,
+          2827.27,
+          2827.27,
+          2827.27,
+          2827.27,
+          2827.27,
+          2827.27
+        ]
+      },
+      {
+        "cohort": "2015Q3",
+        "values": [
+          503.04,
+          714.47,
+          809.71,
+          869.11,
+          1069.86,
+          1280.31,
+          1374.39,
+          1701.37,
+          2147.16,
+          2363.18,
+          2363.18,
+          2363.18,
+          2363.18,
+          2363.18,
+          2363.18,
+          2363.18
+        ]
+      },
+      {
+        "cohort": "2015Q4",
+        "values": [
+          521.18,
+          533.88,
+          638.06,
+          906.97,
+          1098.36,
+          1197.43,
+          1213.89,
+          1538.86,
+          1893.67,
+          1893.67,
+          1893.67,
+          1893.67,
+          1893.67,
+          1893.67,
+          1893.67,
+          1893.67
+        ]
+      },
+      {
+        "cohort": "2016Q1",
+        "values": [
+          350.32,
+          724.85,
+          759.53,
+          954.39,
+          1260.97,
+          1717.54,
+          2107.61,
+          2393.66,
+          2393.66,
+          2393.66,
+          2393.66,
+          2393.66,
+          2393.66,
+          2393.66,
+          2393.66,
+          2393.66
+        ]
+      },
+      {
+        "cohort": "2016Q2",
+        "values": [
+          786.82,
+          1302.44,
+          1544.84,
+          2438.66,
+          2509.63,
+          2622.03,
+          2880.07,
+          2880.07,
+          2880.07,
+          2880.07,
+          2880.07,
+          2880.07,
+          2880.07,
+          2880.07,
+          2880.07,
+          2880.07
+        ]
+      },
+      {
+        "cohort": "2016Q3",
+        "values": [
+          349.14,
+          349.14,
+          571.07,
+          791.89,
+          986.84,
+          1422.6,
+          1422.6,
+          1422.6,
+          1422.6,
+          1422.6,
+          1422.6,
+          1422.6,
+          1422.6,
+          1422.6,
+          1422.6,
+          1422.6
+        ]
+      },
+      {
+        "cohort": "2016Q4",
+        "values": [
+          595.65,
+          780.82,
+          808.01,
+          840.39,
+          1144.61,
+          1144.61,
+          1144.61,
+          1144.61,
+          1144.61,
+          1144.61,
+          1144.61,
+          1144.61,
+          1144.61,
+          1144.61,
+          1144.61,
+          1144.61
+        ]
+      },
+      {
+        "cohort": "2017Q1",
+        "values": [
+          161.68,
+          163.81,
+          420.08,
+          645.04,
+          645.04,
+          645.04,
+          645.04,
+          645.04,
+          645.04,
+          645.04,
+          645.04,
+          645.04,
+          645.04,
+          645.04,
+          645.04,
+          645.04
+        ]
+      },
+      {
+        "cohort": "2017Q2",
+        "values": [
+          32.36,
+          32.36,
+          65.29,
+          65.29,
+          65.29,
+          65.29,
+          65.29,
+          65.29,
+          65.29,
+          65.29,
+          65.29,
+          65.29,
+          65.29,
+          65.29,
+          65.29,
+          65.29
+        ]
+      },
+      {
+        "cohort": "2017Q3",
+        "values": [
+          802.78,
+          882.34,
+          882.34,
+          882.34,
+          882.34,
+          882.34,
+          882.34,
+          882.34,
+          882.34,
+          882.34,
+          882.34,
+          882.34,
+          882.34,
+          882.34,
+          882.34,
+          882.34
+        ]
+      },
+      {
+        "cohort": "2017Q4",
+        "values": [
+          933.04,
+          933.04,
+          933.04,
+          933.04,
+          933.04,
+          933.04,
+          933.04,
+          933.04,
+          933.04,
+          933.04,
+          933.04,
+          933.04,
+          933.04,
+          933.04,
+          933.04,
+          933.04
+        ]
+      }
+    ]
+  },
+  "rfm": {
+    "summary": {
+      "At Risk": {
+        "count": 200,
+        "recency": 257.1,
+        "frequency": 6.66,
+        "monetary": 692546.95
+      },
+      "Champions": {
+        "count": 106,
+        "recency": 25.5,
+        "frequency": 9.31,
+        "monetary": 560498.82
+      },
+      "Hibernating": {
+        "count": 183,
+        "recency": 264.8,
+        "frequency": 4.8,
+        "monetary": 173164.13
+      },
+      "Loyalists": {
+        "count": 158,
+        "recency": 50.7,
+        "frequency": 7.68,
+        "monetary": 569388.86
+      },
+      "Potential Loyalists": {
+        "count": 146,
+        "recency": 45.2,
+        "frequency": 4.1,
+        "monetary": 301602.11
+      }
+    },
+    "top_customers": {
+      "At Risk": [
+        {
+          "id": "TC-20980",
+          "name": "Tamara Chand",
+          "recency": 400,
+          "frequency": 5,
+          "monetary": 19052.22,
+          "segment": "Corporate"
+        },
+        {
+          "id": "SC-20095",
+          "name": "Sanjit Chand",
+          "recency": 350,
+          "frequency": 9,
+          "monetary": 14142.33,
+          "segment": "Consumer"
+        },
+        {
+          "id": "BM-11140",
+          "name": "Becky Martin",
+          "recency": 308,
+          "frequency": 4,
+          "monetary": 11789.63,
+          "segment": "Consumer"
+        },
+        {
+          "id": "CJ-12010",
+          "name": "Caroline Jumper",
+          "recency": 190,
+          "frequency": 8,
+          "monetary": 11164.97,
+          "segment": "Consumer"
+        },
+        {
+          "id": "CL-12565",
+          "name": "Clay Ludtke",
+          "recency": 285,
+          "frequency": 12,
+          "monetary": 10880.55,
+          "segment": "Consumer"
+        }
+      ],
+      "Hibernating": [
+        {
+          "id": "VT-21700",
+          "name": "Valerie Takahito",
+          "recency": 1001,
+          "frequency": 2,
+          "monetary": 1736.6,
+          "segment": "Home Office"
+        },
+        {
+          "id": "AH-10120",
+          "name": "Adrian Hane",
+          "recency": 61,
+          "frequency": 7,
+          "monetary": 1735.51,
+          "segment": "Home Office"
+        },
+        {
+          "id": "MV-18190",
+          "name": "Mike Vittorini",
+          "recency": 20,
+          "frequency": 7,
+          "monetary": 1734.57,
+          "segment": "Consumer"
+        },
+        {
+          "id": "JM-15535",
+          "name": "Jessica Myrick",
+          "recency": 103,
+          "frequency": 7,
+          "monetary": 1733.44,
+          "segment": "Consumer"
+        },
+        {
+          "id": "DB-13405",
+          "name": "Denny Blanton",
+          "recency": 188,
+          "frequency": 4,
+          "monetary": 1711.69,
+          "segment": "Consumer"
+        }
+      ],
+      "Loyalists": [
+        {
+          "id": "RB-19360",
+          "name": "Raymond Buch",
+          "recency": 97,
+          "frequency": 6,
+          "monetary": 15117.34,
+          "segment": "Consumer"
+        },
+        {
+          "id": "HL-15040",
+          "name": "Hunter Lopez",
+          "recency": 44,
+          "frequency": 6,
+          "monetary": 12873.3,
+          "segment": "Consumer"
+        },
+        {
+          "id": "TS-21370",
+          "name": "Todd Sumrall",
+          "recency": 37,
+          "frequency": 6,
+          "monetary": 11891.75,
+          "segment": "Corporate"
+        },
+        {
+          "id": "SV-20365",
+          "name": "Seth Vernon",
+          "recency": 102,
+          "frequency": 10,
+          "monetary": 11470.95,
+          "segment": "Consumer"
+        },
+        {
+          "id": "KF-16285",
+          "name": "Karen Ferguson",
+          "recency": 98,
+          "frequency": 7,
+          "monetary": 10604.27,
+          "segment": "Home Office"
+        }
+      ],
+      "Champions": [
+        {
+          "id": "AB-10105",
+          "name": "Adrian Barton",
+          "recency": 42,
+          "frequency": 10,
+          "monetary": 14473.57,
+          "segment": "Consumer"
+        },
+        {
+          "id": "KL-16645",
+          "name": "Ken Lonsdale",
+          "recency": 48,
+          "frequency": 12,
+          "monetary": 14175.23,
+          "segment": "Consumer"
+        },
+        {
+          "id": "SE-20110",
+          "name": "Sanjit Engle",
+          "recency": 10,
+          "frequency": 11,
+          "monetary": 12209.44,
+          "segment": "Consumer"
+        },
+        {
+          "id": "GT-14710",
+          "name": "Greg Tran",
+          "recency": 37,
+          "frequency": 11,
+          "monetary": 11820.12,
+          "segment": "Consumer"
+        },
+        {
+          "id": "ME-17320",
+          "name": "Maria Etezadi",
+          "recency": 43,
+          "frequency": 10,
+          "monetary": 10663.73,
+          "segment": "Home Office"
+        }
+      ],
+      "Potential Loyalists": [
+        {
+          "id": "SM-20320",
+          "name": "Sean Miller",
+          "recency": 80,
+          "frequency": 5,
+          "monetary": 25043.05,
+          "segment": "Home Office"
+        },
+        {
+          "id": "TA-21385",
+          "name": "Tom Ashbrook",
+          "recency": 70,
+          "frequency": 4,
+          "monetary": 14595.62,
+          "segment": "Home Office"
+        },
+        {
+          "id": "CC-12370",
+          "name": "Christopher Conant",
+          "recency": 44,
+          "frequency": 5,
+          "monetary": 12129.07,
+          "segment": "Consumer"
+        },
+        {
+          "id": "GT-14635",
+          "name": "Grant Thornton",
+          "recency": 57,
+          "frequency": 3,
+          "monetary": 9351.21,
+          "segment": "Corporate"
+        },
+        {
+          "id": "CM-12385",
+          "name": "Christopher Martinez",
+          "recency": 38,
+          "frequency": 4,
+          "monetary": 8954.02,
+          "segment": "Consumer"
+        }
+      ]
+    }
+  }
+};
